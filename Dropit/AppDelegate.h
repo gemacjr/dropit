a9ea065dbc8634e130dc2480caa9ddddf59b3290
Ed McCormic <edmccormic@gmail.com>
2015-05-27 20:48:07 -0600

    Initial Commit

---
+++ Dropit/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Dropit
+//
+//  Created by ED on 5/27/15.
+//  Copyright (c) 2015 SwiftBeard. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
